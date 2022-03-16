605fb5e708147fb767fa9e083ec9cd6e5edd3e5d
Amanda <amandaroblesurena@gmail.com>
2022-03-16 20:18:52 +0100

    Version 1 de Dev1

--- src/Ejercicio01.java
+++ src/Ejercicio01.java
@@ -2,8 +2,10 @@
 public class Ejercicio01 {
 	
  public static void main(String[] args) {
+	 System.out.println("otro");
 	 System.out.println("Hola");
 	 System.out.println("Te vuelvo a saludar");
+	 System.out.println("Hola desde Dev1");
  }
 
 
